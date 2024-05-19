0f27cd8c35b20a5ef2d80e18706d667b35e04dcf
Ugo <nozolo96@gmail.com>
2024-05-19 17:30:21 +0100

    refactor: Move sine table to another file

---
+++ Core/Inc/ugo_sine.h
@@ -0,0 +1,15 @@
+/*
+ * ugo_sine.h
+ *
+ *  Created on: May 19, 2024
+ *      Author: nozol
+ */
+
+#ifndef INC_UGO_SINE_H_
+#define INC_UGO_SINE_H_
+
+extern int sine_vals[320];
+
+
+
+#endif /* INC_UGO_SINE_H_ */
